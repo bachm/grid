b114f445833e29bfe9132bd7334d7ef397f88ef6
an <an@an-pc.lan>
2015-06-03 21:34:15 +0200

    updated

--- src/lib.rs
+++ src/lib.rs
@@ -3,39 +3,51 @@
 extern crate alloc;
 
 use self::alloc::heap;
-
 use std::mem;
 use std::ptr;
 use std::iter::Iterator;
 use std::marker::PhantomData;
 use std::slice;
+use std::ops::{Index, IndexMut};
 
-/// A two-dimensional array with whose dimensions are fixed at construction.
-pub struct Grid<T> {
+/// A dynamically allocated two-dimensional array with fixed size.
+pub struct DynArray2<T> {
     ptr: *mut T,
     width: u16,
-    height: u16,
+    height: u16
 }
 
-impl<T: Clone> Grid<T> {
-    /// Constructs a grid with the given width and height by cloning `element`.
-    /// Returns None if width, height or size of T are zero.
-    pub fn new(width: u16, height: u16, element: T) -> Option<Grid<T>> {
+impl<T: Clone> DynArray2<T> {
+    /// Constructs an array with the given width and height by cloning `element`.
+    /// Will panic if width, height or size of T are zero, or allocation fails.
+    pub fn new(width: u16, height: u16, element: T) -> DynArray2<T> {
+        let ptr = DynArray2::init(width, height, element).expect("DynArray2::new called with invalid input.");
+        DynArray2 { ptr: ptr, width: width, height: height }
+    }
+    
+    /// Constructs an array with the given width and height by cloning `element`.
+    /// Will return None if width, height or size of T are zero, or allocation fails.
+    pub fn new_opt(width: u16, height: u16, element: T) -> Option<DynArray2<T>> {
+        DynArray2::init(width, height, element).map(|ptr| DynArray2 { ptr: ptr, width: width, height: height })
+    }
+    
+    fn init(width: u16, height: u16, element: T) -> Option<*mut T> {
         if mem::size_of::<T>() > 0 && width > 0 && height > 0 {
             let count = width as usize * height as usize;
             let bytes = count * mem::size_of::<T>();
             let ptr = unsafe { heap::allocate(bytes, mem::align_of::<T>()) as *mut T };
+            if ptr.is_null() { return None }
             for i in 0..count as isize {
                 unsafe { ptr::write(ptr.offset(i), element.clone()); };
             }
-            Some(Grid { ptr: ptr, width: width, height: height })
+            Some(ptr)
         } else {
             None
         }
     }
 }
 
-impl<T> Grid<T> {    
+impl<T> DynArray2<T> {    
     pub fn iter<'a>(&'a self) -> Iter<'a, T> {
         Iter { ptr: self.ptr, end: self.end(), marker: PhantomData }
     }
@@ -69,11 +81,11 @@ impl<T> Grid<T> {
     }
     
     pub fn width(&self) -> u16 {
-       self.width
+        self.width
     }
     
     pub fn height(&self) -> u16 {
-       self.height
+        self.height
     }
     
     #[inline]
@@ -87,12 +99,14 @@ impl<T> Grid<T> {
     }
 }
 
-impl<T> Drop for Grid<T> {
+impl<T> Drop for DynArray2<T> {
     fn drop(&mut self) {
         unsafe {
-           for e in self.iter_mut() { ptr::read(e); }
-           let bytes = self.width as usize * self.height as usize * mem::size_of::<T>();
-           heap::deallocate(self.ptr as *mut u8, bytes, mem::align_of::<T>());
+            for e in self.iter_mut() { 
+                ptr::read(e);
+            }
+            let bytes = self.width as usize * self.height as usize * mem::size_of::<T>();
+            heap::deallocate(self.ptr as *mut u8, bytes, mem::align_of::<T>());
         }
     }
 }
@@ -175,38 +189,68 @@ impl<'a, T> Iterator for RowsMut<'a, T> {
     }
 }
 
+const INDEX_ERROR_MSG: &'static str = "Attempted to index DynArray2 with invalid input."; 
+
+impl<T> Index<(u16, u16)> for DynArray2<T> {
+    type Output = T;
+    
+    fn index(&self, (x, y): (u16, u16)) -> &T {
+        if x < self.width && y < self.height {
+            unsafe {
+                mem::transmute(self.ptr.offset(x as isize + y as isize * self.width as isize))
+            }
+        } else {
+            panic!(INDEX_ERROR_MSG)
+        }
+    }
+}
+
+impl<T> IndexMut<(u16, u16)> for DynArray2<T> {    
+    fn index_mut(&mut self, (x, y): (u16, u16)) -> &mut T {
+        if x < self.width && y < self.height {
+            unsafe {
+                mem::transmute(self.ptr.offset(x as isize + y as isize * self.width as isize))
+            }
+        } else {
+            panic!(INDEX_ERROR_MSG)
+        }
+    }
+}
+
+
 #[cfg(test)]
-mod test {
+#[allow(unused_variables)]
+mod test {    
     extern crate core;
-    use super::Grid;
+    use super::DynArray2;
     use self::core::array::FixedSizeArray;
     use std::cell::Cell;
     
     #[test]
     fn get_get_mut() {
-        let mut grid = Grid::new(2, 2, 0u8).unwrap();
-        assert_eq!(grid.get(0, 0), Some(&0u8));
-        assert_eq!(grid.get(0, 1), Some(&0u8));
-        assert_eq!(grid.get(1, 0), Some(&0u8));
-        assert_eq!(grid.get(1, 1), Some(&0u8));
-        assert_eq!(grid.get(2, 0), None);
-        *grid.get_mut(1, 0).unwrap() = 1u8;
-        *grid.get_mut(0, 1).unwrap() = 2u8;
-        *grid.get_mut(1, 1).unwrap() = 3u8;
-        assert_eq!(grid.get(0, 0), Some(&0u8));
-        assert_eq!(grid.get(1, 0), Some(&1u8));
-        assert_eq!(grid.get(0, 1), Some(&2u8));
-        assert_eq!(grid.get(1, 1), Some(&3u8));
-        assert_eq!(grid.get(2, 0), None);
+        let mut array = DynArray2::new(2, 2, 0u8);
+        assert_eq!(array.get(0, 0), Some(&0u8));
+        assert_eq!(array.get(0, 1), Some(&0u8));
+        assert_eq!(array.get(1, 0), Some(&0u8));
+        assert_eq!(array.get(1, 1), Some(&0u8));
+        assert_eq!(array.get(2, 0), None);
+        *array.get_mut(1, 0).unwrap() = 1u8;
+        *array.get_mut(0, 1).unwrap() = 2u8;
+        *array.get_mut(1, 1).unwrap() = 3u8;
+        assert_eq!(array.get(0, 0), Some(&0u8));
+        assert_eq!(array.get(1, 0), Some(&1u8));
+        assert_eq!(array.get(0, 1), Some(&2u8));
+        assert_eq!(array.get(1, 1), Some(&3u8));
+        assert_eq!(array.get(2, 0), None);
     }
     
     #[test]
     fn iter() {
-        let mut grid = Grid::new(2, 2, 0u8).unwrap();
-        *grid.get_mut(1, 0).unwrap() = 1;
-        *grid.get_mut(0, 1).unwrap() = 2;
-        *grid.get_mut(1, 1).unwrap() = 3;
-        let mut iter = grid.iter();
+        let mut array = DynArray2::new(2, 2, 0u8);
+        *array.get_mut(1, 0).unwrap() = 1;
+        *array.get_mut(0, 1).unwrap() = 2;
+        *array.get_mut(1, 1).unwrap() = 3;
+        let mut iter = array.iter();
         assert_eq!(iter.next(), Some(&0));
         assert_eq!(iter.next(), Some(&1));
         assert_eq!(iter.next(), Some(&2));
@@ -216,11 +260,11 @@ mod test {
         
     #[test]
     fn iter_mut() {
-        let mut grid = Grid::new(2, 2, 0u8).unwrap();
-        *grid.get_mut(1, 0).unwrap() = 1;
-        *grid.get_mut(0, 1).unwrap() = 2;
-        *grid.get_mut(1, 1).unwrap() = 3;
-        let mut iter = grid.iter_mut();
+        let mut array = DynArray2::new(2, 2, 0u8);
+        *array.get_mut(1, 0).unwrap() = 1;
+        *array.get_mut(0, 1).unwrap() = 2;
+        *array.get_mut(1, 1).unwrap() = 3;
+        let mut iter = array.iter_mut();
         assert_eq!(iter.next(), Some(&mut 0));
         assert_eq!(iter.next(), Some(&mut 1));
         assert_eq!(iter.next(), Some(&mut 2));
@@ -230,11 +274,11 @@ mod test {
     
     #[test]
     fn rows() {
-        let mut grid = Grid::new(2, 2, 'A').unwrap();
-        *grid.get_mut(1, 0).unwrap() = 'B';
-        *grid.get_mut(0, 1).unwrap() = 'C';
-        *grid.get_mut(1, 1).unwrap() = 'D';
-        let mut iter = grid.rows();
+        let mut array = DynArray2::new(2, 2, 'A');
+        *array.get_mut(1, 0).unwrap() = 'B';
+        *array.get_mut(0, 1).unwrap() = 'C';
+        *array.get_mut(1, 1).unwrap() = 'D';
+        let mut iter = array.rows();
         assert_eq!(iter.next(), Some(['A', 'B'].as_slice()));
         assert_eq!(iter.next(), Some(['C', 'D'].as_slice()));
         assert_eq!(iter.next(), None);
@@ -242,11 +286,11 @@ mod test {
     
     #[test]
     fn rows_mut() {
-        let mut grid = Grid::new(2, 2, 'A').unwrap();
-        *grid.get_mut(1, 0).unwrap() = 'B';
-        *grid.get_mut(0, 1).unwrap() = 'C';
-        *grid.get_mut(1, 1).unwrap() = 'D';
-        let mut iter = grid.rows_mut();
+        let mut array = DynArray2::new(2, 2, 'A');
+        *array.get_mut(1, 0).unwrap() = 'B';
+        *array.get_mut(0, 1).unwrap() = 'C';
+        *array.get_mut(1, 1).unwrap() = 'D';
+        let mut iter = array.rows_mut();
         assert_eq!(iter.next(), Some(['A', 'B'].as_mut_slice()));
         assert_eq!(iter.next(), Some(['C', 'D'].as_mut_slice()));
         assert_eq!(iter.next(), None);
@@ -264,18 +308,44 @@ mod test {
         }
         let count = Cell::new(0u8);
         {
-            let grid = Grid::new(2, 2, Foo(&count));
+            let array = DynArray2::new(2, 2, Foo(&count));
         }
-        // Four allocations plus the Foo passed to Grid::new.
+        // Four allocations plus the Foo passed to DynArray2::new.
         assert_eq!(count.get(), 5);
     }
     
     #[test]
-    fn new_with_bad_input() {
+    #[should_panic]
+    fn phantom_type() {
         #[derive(Copy, Clone)]
         struct Foo;
-        assert!(Grid::new(3, 0, 1u8).is_none());
-        assert!(Grid::new(0, 3, 1u8).is_none());
-        assert!(Grid::new(3, 3, Foo).is_none());
+        let array = DynArray2::new(3, 3, Foo);
+    }
+    
+    #[test]
+    #[should_panic]
+    fn bad_width() {
+        let array = DynArray2::new(0, 3, 0u8);
+    }
+    
+    #[test]
+    #[should_panic]
+    fn bad_height() {
+        let array = DynArray2::new(3, 0, 0u8);
+    }
+    
+    #[test]
+    fn indexing() {
+        let mut array = DynArray2::new(2, 2, 0u8);
+        array[(1, 0)] = 23;
+        array[(0, 1)] = 45;
+        assert_eq!(array[(1, 1)], 0u8);
+    }
+    
+    #[test]
+    #[should_panic]
+    fn bad_indexing() {
+        let array = DynArray2::new(2, 2, 0u8);
+        let x = array[(3, 1)];
     }
 }
